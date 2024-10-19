dc34402661f41e9eb11a50af10de88ba6619ae93
Dfn2502 <115895102+Dfn2502@users.noreply.github.com>
2024-10-18 23:19:42 -0600

    Add files via upload

---
+++ java/script_carro_compras.js
@@ -0,0 +1,7 @@
+var count = 0;
+
+function myfunction(){
+    const contador = document.getElementById('contador');
+    count++;
+    contador.textContent = count;
+}
---
+++ java/script_nav_menu.js
@@ -0,0 +1,5 @@
+function toggleMenu() {
+    const menu = document.getElementById('menu');
+    var click = 0;
+    menu.classList.toggle('active');
+}
